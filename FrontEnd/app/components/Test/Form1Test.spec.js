c353a44c8623a6c6985b14664b02b6679f5f7a2c
yizong98 <yizong@ucsd.edu>
2020-07-16 16:21:54 -0700

    Create Form1Test.spec.js

---
+++ FrontEnd/app/components/Test/Form1Test.spec.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import Form1 from "../Form1";
+
+/**
+ * Test Cases for the component Form1.js
+ * Contains tests of the following:
+ *
+ */
+
+/* TODO: test for address 
+case 1: existing address
+case 2: non-existent address
+case 3: default
+*/
