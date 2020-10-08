0e6ea1aba41dbbe1a5c73342e3798c4c73f4fceb
Keenan Chan <keenanjchan@gmail.com>
2020-10-08 11:52:47 -0700

    added simple cookie upon login

--- NewFrontEnd/src/components/Login.tsx
+++ NewFrontEnd/src/components/Login.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import GoogleLogin from 'react-google-login';
-
-const responseGoogle = (response: Response) => {
-  console.log(response);
-};
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from 'react-google-login';
+import { useCookies } from 'react-cookie';
+import moment from 'moment';
 
 // https://developers.google.com/identity/sign-in/web/sign-in
 interface PathProps {
@@ -15,6 +17,32 @@ interface PathProps {
 }
 
 const Login: React.FC<PathProps> = ({ handleClose, show }) => {
+  const [cookies, setCookie] = useCookies(['user']);
+
+  const responseGoogleFail = (response: Error) => {
+    console.log(response);
+  };
+
+  const responseGoogleSuccess = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline,
+  ) => {
+    if (isOnline(response)) {
+      setCookie('user', response.profileObj, {
+        expires: moment().add(30, 'seconds').toDate(), // expires 1 minute after login
+      });
+    } else {
+      console.log(response);
+    }
+  };
+
+  const isOnline = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline,
+  ): response is GoogleLoginResponse => {
+    return 'profileObj' in response;
+  };
+
+  console.log(cookies.user);
+
   return (
     <Modal id="LoginModal" show={show} onHide={handleClose} centered>
       <Button className="btn-filter">
@@ -26,10 +54,17 @@ const Login: React.FC<PathProps> = ({ handleClose, show }) => {
         />
       </Button>
       <img className="d-block" src="/login.svg" alt="LogIn" />
+      {cookies.user !== undefined ? (
+        <span className="word"> Logged In as {cookies.user.givenName}! </span>
+      ) : (
+        <></>
+      )}
       <GoogleLogin
         className="g-auth"
         clientId="778916194800-977823s60p7mtu1sj72ru0922p2pqh6m.apps.googleusercontent.com"
-        onFailure={responseGoogle}
+        onSuccess={responseGoogleSuccess}
+        onFailure={responseGoogleFail}
+        // TODO: add login cookie to onSuccess using react-cookie
         cookiePolicy="single_host_origin"
         icon={false}
       >
--- NewFrontEnd/src/index.tsx
+++ NewFrontEnd/src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/sass/main.scss';
 import App from './App';
+import { CookiesProvider } from 'react-cookie';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <CookiesProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </CookiesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
