74487b84acfcaaffc1dbd0aeddc8934f6fbbb164
Adam Kreitzman <akreitzm@ucsd.edu>
2020-10-22 18:25:13 -0700

    Update database_setup.py

--- BackEnd/app/db/database_setup.py
+++ BackEnd/app/db/database_setup.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, FLOAT, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String,FLOAT
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship,backref
+from sqlalchemy import create_engine
 
 Base = declarative_base()
 
@@ -10,107 +11,132 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    date_created = Column(DateTime, nullable=False)
-    school_year = Column(String(14))
-    major = Column(String(250))
-    phone = Column(String(14))
+    phone = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
-    description = Column(String(500))
-    avatar_photo = Column(String(500))  # photo=url
-    tenant_id = Column(Integer, ForeignKey('tenant.id'))
-    host_id = Column(Integer, ForeignKey('host.id'))
+    description = Column(String(1000), nullable=False)
+    avatar_picture = relationship("PicturePerson", backref="user")
+    photo = relationship("Photo", backref="user")
+    room = relationship("Room", backref="user")
 
-
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'email': self.email,
+            'phone': self.phone
+            'name': self.name
+            'description': self.description,
+            'avatar_picture': self.avatar_picture
+        }
 class Room(Base):
     __tablename__ = 'room'
 
     id = Column(Integer, primary_key=True)
-    intro = Column(String(2500), nullable=False)
-    price_range = Column(String(250), nullable=False)
-    stay_period = Column(String(250), nullable=False)
-    move_time = Column(String(250), nullable=False)
-    name = Column(String(250), nullable=False)
-    lat = Column(FLOAT)
-    lng = Column(FLOAT)
     user_id = Column(Integer, ForeignKey('user.id'))
-    picture = relationship("PictureRoom", backref="room")
-    others = relationship("Others", backref="room")
+    room_type = Column(String(250), nullable=False)
+    move_in = Column(String(250), nullable=False)
+    price = Column(Integer, nullable=False)
+    description = Column(String(1000), nullable=False)
+    stay_period = Column(String(250), nullable=False)
+    distance = Column(FLOAT)
+    address = Column(String(250), nullable=False)
+    photo_url = Column(String(250), ForeignKey="photo.url")
+    move_in_id = Column(Integer, ForeignKey="move_in.id")
+    user = relationship("User",backref="room")
+    photo = relationship("Photo",backref="room")
+    others = relationship("Others",backref="room")
+    move_in = relationship("Move_In",backref="room")
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'room_type': self.room_type
+            'move_in': self.move_in
+            'intro': self.intro,
+            'price': self.price,
+            'description': self.description,
+            'stay_period': self.stay_period,
+            'distance': self.distance,
+            'address': self.address,
+            'photo_url': self.photo_url
+        }
+
+class Photo(Base):
+    __tablename__ = 'photo'
+    
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User",backref="photo")
+    
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'url': self.url,
+            'user_id': self.user_id
+        }
+    
+class Move_In(Base):
+    __tablename__ = 'move_in'
+    
+    id = Column(Integer, primary_key=True)
+    early_interval = Column(String(250), nullable=False)
+    early_month = Column(String(250), nullable=False)
+    late_interval = Column(String(250), nullable=False)
+    late_month = Column(String(250), nullable=False)
+    room_id = Column(Integer, ForeignKey('room.id'))
+    room = relationship("Room", backref="move_in")
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'early_interval': self.early_interval,
+            'early_month': self.early_month,
+            'late_interval': self.late_interval,
+            'late_month': self.late_month,
+            'room_id': self.room_id
+        }
+
+class House_Attribute(Base):
+    __tablename__ = 'house_attribute'
+    
+    id = Column(Integer, primary_key=True)
+    room_id = Column(Integer, ForeignKey('room.id'))
+    attribute_name = Column(String(250), nullable=False)
+    
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'room_id': self.room_id,
+            'attribute_name': self.attribute_name
+        }
+    
+class Attribute(Base):
+    __tablename__ = 'attribute'
+    
+    name = Column(String(250), primary_key=True)
+    category = Column(String(250), nullable=False)
+    house_attribute_id = Column(Integer, ForeignKey('house_attribute.id'))
+    house_attribute = relationship("House_Attribute", backref="user")
+    
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
             'name': self.name,
-            'id': self.id,
-            'intro': self.intro,
-            'price_range': self.price_range,
-            'stay_period': self.stay_period,
-            'move_time': self.move_time,
-            'lat': self.lat,
-            'lng': self.lng
+            'category': self.category,
+            'house_attribute_id': self.house_attribute_id
         }
-
-
-class Tenant(Base):
-    __tablename__ = 'tenant'
-
-    id = Column(Integer, primary_key=True)
-    intro = Column(String(2500), nullable=False)
-    price_range = Column(String(250), nullable=False)
-    stay_period = Column(String(250), nullable=False)
-    move_time = Column(String(250), nullable=False)
-    r_type = Column(String(250), nullable=False)
-    picture = relationship("PicturePerson", backref="tenant")
-    others = relationship("OthersT", backref="tenant")
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'r_type': self.r_type,
-            'id': self.id,
-            'intro': self.intro,
-            'price_range': self.price_range,
-            'stay_period': self.stay_period,
-            'move_time': self.move_time
-        }
-
-
-class PictureRoom(Base):
-    __tablename__ = 'pictureroom'
-
-    id = Column(Integer, primary_key=True)
-    picture = Column(String(250))
-    type_n = Column(String(250))
-    room_id = Column(Integer, ForeignKey('room.id'), default=-1)
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-
-class PicturePerson(Base):
-    __tablename__ = 'pictureperson'
-
-    id = Column(Integer, primary_key=True)
-    type_n = Column(String(250))
-    picture = Column(String(250))
-    tenant_id = Column(Integer, ForeignKey('tenant.id'), default=-1)
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-
-class Others(Base):
-    __tablename__ = 'others'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    room_id = Column(Integer, ForeignKey('room.id'))
-
-
-class OthersT(Base):
-    __tablename__ = 'otherst'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    tenant_id = Column(Integer, ForeignKey('tenant.id'))
-
 
 engine = create_engine('sqlite:///housing.db')
 
