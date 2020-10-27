48259639fb4507f7fd7d33f9d49bd7ae6ccd13d4
Keenan Chan <keenanjchan@gmail.com>
2020-10-27 01:40:11 -0700

    Added implementation of redux login/logout flow

--- NewFrontEnd/src/components/NavBar.tsx
+++ NewFrontEnd/src/components/NavBar.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Container, Col, Row } from 'react-bootstrap';
 import { GoogleLogout } from 'react-google-login';
 import { useCookies } from 'react-cookie';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser, selectUser, removeUser } from '../redux/slices/auth';
 import Login from './Login';
 import { navIcons } from '../assets/icons/all';
 
@@ -17,6 +19,17 @@ const NavBar: React.FC = () => {
 
   const [cookies, setCookie, removeCookie] = useCookies(['user']);
 
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    if (cookies.user !== undefined && user === undefined) {
+      const { name, email, imageUrl, ...rest } = cookies.user;
+      const userFromCookie = { name, email, imageUrl };
+      dispatch(setUser(userFromCookie));
+    }
+  });
+
   return (
     <Container fluid className="d-flex navbar-whole">
       <Login show={showLogin} handleClose={handleCloseLogin} />
@@ -30,7 +43,7 @@ const NavBar: React.FC = () => {
       </Col>
       <Col md={3}>
         <Row className="h-25" />
-        {cookies.user === undefined ? (
+        {user === undefined ? (
           <Button className="g-sign-in" onClick={handleShowLogin}>
             {/* need to handle isSignedOut case */}
             Sign In
@@ -40,6 +53,7 @@ const NavBar: React.FC = () => {
             clientId="778916194800-977823s60p7mtu1sj72ru0922p2pqh6m.apps.googleusercontent.com"
             onLogoutSuccess={() => {
               removeCookie('user');
+              dispatch(removeUser());
             }}
             render={(renderProps) => (
               <Button
