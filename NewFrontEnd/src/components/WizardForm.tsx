eb1d8b18447934aec2cab0649484a3bf29f987d5
Amit Bar <amitbar00@gmail.com>
2020-12-21 18:03:00 -0800

    made newer version of wizard form without use hook. now taking it apart even further

--- NewFrontEnd/src/components/WizardForm.tsx
+++ NewFrontEnd/src/components/WizardForm.tsx
@@ -1,4 +1,4 @@
-// TODO eventually separate the zod stuff in this into a separate
+// TODO eventually separate the zod stuff in this into a separate function
 /* Documentation of this component is at the bottom */
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import z, { ZodSchema, ZodIssue } from 'zod';
+import { ZodSchema, ZodIssue } from 'zod';
 import { miscIcons } from '../assets/icons/all';
 
 type ValidationError<P> = Partial<
@@ -22,19 +22,22 @@ type ValidationError<P> = Partial<
  * - nextStep()/prevStep() function to navigate the form
  * - an exit() function to exit the form (i.e. when the user completes the form)
  * - a submitForm() which can be called when you would like to submit the form (it returns T/F based on success of onSubmit and validationChecks)
+ * // TODO
  */
-type SetStore<P> = (value: Partial<P>, shouldValidate?: boolean) => void;
-export interface WizardFormStep<P> {
+type SubmitForm = () => Promise<{ success: boolean; message?: string }>;
+type SetStore<P> = (value: Partial<P>) => void;
+type SetSchema = (schema: ZodSchema<any>) => void;
+type SetInitialStore = <P extends {}>(value: Partial<P>) => void;
+export type WizardFormStep<P> = P & {
   exitWizardForm: () => void;
   nextStep: () => void;
   prevStep: () => void;
-  useWizardFormStorage: <P extends {}>(
-    initialValue?: P,
-    schema?: ZodSchema<P>,
-  ) => [P, ValidationError<P> | undefined, SetStore<P>];
-  submitForm: () => Promise<ValidationError<P> | { success: boolean }>; // TODO returns success or failure
-  overrideValidation: (validated: boolean) => void;
-}
+  submitForm: SubmitForm; // returns success or failure with a message
+  errors: ValidationError<P> | undefined; // validation errors
+  setStore: SetStore<P>;
+  setSchema: SetSchema;
+  setInitialStore: SetInitialStore;
+};
 
 // T is whatever is in the store
 interface PathProps<T = {}> {
@@ -43,8 +46,7 @@ interface PathProps<T = {}> {
   setShow: (show: boolean) => void;
   hideButtons?: boolean;
   onSubmit: (store: T) => boolean;
-  // TODO validateOnlyAtSubmit?: boolean;
-  // TODO initialStorage?: Partial<T>;
+  // TODO initialStore?: Partial<T>;
 }
 
 /**
@@ -57,27 +59,25 @@ const WizardForm = <T extends {}>({
   setShow,
   hideButtons = false,
   onSubmit,
-}: // TODO validateOnlyAtSubmit = false,
-// TODO initialStorage = {},
+}: // TODO initialStore,
 PathProps<T>) => {
   const [index, setIndex] = useState<number>(0);
   const [isFirst, setIsFirst] = useState<boolean>(true);
   const [isLast, setIsLast] = useState<boolean>(index === children.length - 1);
   const [CurStep, setCurStep] = useState<React.ReactElement>(children[0]);
 
-  const [store, setStore] = useState<Array<Partial<T>>>(
+  const [store, setCompleteStore] = useState<Array<Partial<T>>>(
     children.map(() => ({})),
   );
   // TODO const [curStore, setCurStore] = useState<>
-  // need to keep track of when the store is initialized (otherwise, it'll be an infinite loop of reupdating)
-  const [storeInitialized, setStoreInitialized] = useState<Array<boolean>>(
-    children.map(() => false),
-  );
 
   const [zodSchemas, setZodSchemas] = useState<
     Array<ZodSchema<Partial<T>> | undefined>
   >(children.map(() => undefined));
   // need to keep track of when the store is initialized (otherwise, it'll be an infinite loop of reupdating)
+  const [storeInitialized, setStoreInitialized] = useState<Array<boolean>>(
+    children.map(() => false),
+  );
   const [schemasInitialized, setSchemasInitialized] = useState<Array<boolean>>(
     children.map(() => false),
   );
@@ -85,14 +85,6 @@ PathProps<T>) => {
   const [errors, setErrors] = useState<
     Array<ValidationError<Partial<T>> | undefined>
   >(children.map(() => undefined));
-  // used to keep track of which fields have been edited
-  const [editedFields, setEditedFields] = useState<
-    Array<Partial<{ [key in keyof T]: boolean }>>
-  >(children.map(() => ({})));
-  // TODO used to override validations if necessary
-  // const [validationsOverride, setValidationsOverride] = useState<
-  //   Array<boolean | undefined>
-  // >(children.map(() => undefined));
 
   useEffect(() => {
     setCurStep(children[index]);
@@ -132,14 +124,9 @@ PathProps<T>) => {
    * (i.e. returns false if any validations are false, returns false
    * if onSubmit returns false, otherwise returns true).
    */
-  const submitForm = async (): Promise<
-    ValidationError<T> | { success: boolean }
-  > => {
-    // TODO const allValidations = validationsOverride.reduce<boolean>(
-    //   (prev, cur) => prev && (cur === undefined || cur),
-    //   true,
-    // );
-    // if (!allValidations) return { success: false /* errors: {} */ }; // TODO temporary
+  const submitForm: SubmitForm = async () => {
+    // validateCurrent // TODO handle this case
+    // if (validate errors exist) return; // TODO
 
     // Everything should be validated by this point
     const combined = store.reduceRight((pre, cur) => ({ ...pre, ...cur }));
@@ -178,7 +165,6 @@ PathProps<T>) => {
    * Validates current form step.
    */
   const validateCurrent = () => {
-    // TODO temporary "T", should actually be P
     // validate everything that hasn't been validated yet
     let changedErrors: ValidationError<Partial<T>> | undefined;
     console.log(index);
@@ -194,76 +180,47 @@ PathProps<T>) => {
   };
   // need to validate on arrows. need to pass errors
 
-  /**
-   * Hook to access function to update WizardForm's "local store". It's shared among all children.
-   * Works similar to useState, except also can provide zod schema for validation here.
-   */
-  const useWizardFormStorage = <P extends Partial<T>>(
-    initialValue: P,
-    schema?: ZodSchema<P>,
-  ) => {
-    const setStoreWrapper: SetStore<P> = (
-      value: Partial<P>,
-      shouldValidate = true,
-    ) => {
-      const changedValues = { ...store[index], ...value };
-      setStore({ ...store, [index]: changedValues });
+  const setStore: SetStore<T> = (value: Partial<T>) => {
+    const changedValues = { ...store[index], ...value };
+    setCompleteStore({ ...store, [index]: changedValues });
 
-      // get the changed edited fields and set that they were changed
-      const changedEditedFields = (Object.keys(value) as Array<keyof P>).reduce<
-        Partial<{ [key in keyof P]: boolean }>
-      >((prev, key) => {
-        prev[key] = true;
-        return prev;
-      }, {});
-      setEditedFields({
-        ...editedFields,
-        [index]: { ...editedFields[index], changedEditedFields },
-      });
+    // get the changed edited fields and set that they were changed
+    const changedEditedFields = (Object.keys(value) as Array<keyof T>).reduce<
+      Partial<{ [key in keyof T]: boolean }>
+    >((prev, key) => {
+      prev[key] = true;
+      return prev;
+    }, {});
 
-      // every time there's a change, validate it
-      let changedErrors: ValidationError<P> | undefined;
-      if (schema && shouldValidate) {
-        changedErrors = validatePickedValues<P>(
-          schema,
-          changedValues,
-          Object.keys(changedEditedFields) as (keyof P)[],
-        );
-      }
+    // every time there's a change, validate it
+    let changedErrors: ValidationError<T> | undefined;
+    const schema = zodSchemas[index];
+    if (schema) {
+      changedErrors = validatePickedValues<Partial<T>>(
+        schema,
+        changedValues,
+        Object.keys(changedEditedFields) as (keyof T)[],
+      );
+    }
 
-      setErrors({ ...errors, [index]: { ...errors[index], ...changedErrors } });
-    };
+    setErrors({ ...errors, [index]: { ...errors[index], ...changedErrors } });
+  };
 
+  const setSchema: SetSchema = <P extends Partial<T>>(schema: ZodSchema<P>) => {
     if (!schemasInitialized[index]) {
       setZodSchemas({ ...zodSchemas, [index]: schema });
       setSchemasInitialized({ ...schemasInitialized, [index]: true });
     }
-
-    if (!storeInitialized[index]) {
-      if (initialValue) {
-        setStoreWrapper(initialValue, false);
-      }
-      setStoreInitialized({ ...storeInitialized, [index]: true });
-    }
-
-    return [store[index] as P & T, errors[index], setStoreWrapper] as [
-      Partial<P>,
-      ValidationError<P> | undefined,
-      SetStore<P>,
-    ];
   };
 
-  /**
-   * Use this to override if the current step is validated.
-   * NOTE: you can only set isValidated in the current page.
-   */
-  // TODO const overrideValidation = (validated: boolean) => {
-  //   const updatedValidations = [
-  //     ...validationsOverride,
-  //   ] as typeof validationsOverride;
-  //   updatedValidations[index] = validated;
-  //   setValidationsOverride(updatedValidations);
-  // };
+  const setInitialStore: SetInitialStore = <P extends Partial<T>>(
+    value: Partial<P>,
+  ) => {
+    if (!storeInitialized[index]) {
+      setCompleteStore({ ...store, [index]: { ...value } as Partial<T> });
+      setStoreInitialized({ ...storeInitialized, [index]: true });
+    }
+  };
 
   // TODO need to figure out how to have loading thing on top
   return (
@@ -291,8 +248,10 @@ PathProps<T>) => {
               prevStep,
               exitWizardForm,
               submitForm,
-              useWizardFormStorage,
-              // TODO overrideValidation,
+              setStore,
+              setSchema,
+              setInitialStore,
+              ...store[index],
             })}
           </Col>
 
@@ -442,3 +401,62 @@ export const FakeStepTest4: React.FC = () => (
     </Row>
   </Container>
 );
+
+/**
+ * Hook to access function to update WizardForm's "local store". It's shared among all children.
+ * Works similar to useState, except also can provide zod schema for validation here.
+ */
+// const useWizardFormStorage = <P extends Partial<T>>(
+//   initialValue: P,
+//   schema?: ZodSchema<P>,
+// ) => {
+//   const setStoreWrapper: SetStore<P> = (
+//     value: Partial<P>,
+//     shouldValidate = true,
+//   ) => {
+//     const changedValues = { ...store[index], ...value };
+//     setCompleteStore({ ...store, [index]: changedValues });
+
+//     // get the changed edited fields and set that they were changed
+//     const changedEditedFields = (Object.keys(value) as Array<keyof P>).reduce<
+//       Partial<{ [key in keyof P]: boolean }>
+//     >((prev, key) => {
+//       prev[key] = true;
+//       return prev;
+//     }, {});
+//     setEditedFields({
+//       ...editedFields,
+//       [index]: { ...editedFields[index], changedEditedFields },
+//     });
+
+//     // every time there's a change, validate it
+//     let changedErrors: ValidationError<P> | undefined;
+//     if (schema && shouldValidate) {
+//       changedErrors = validatePickedValues<P>(
+//         schema,
+//         changedValues,
+//         Object.keys(changedEditedFields) as (keyof P)[],
+//       );
+//     }
+
+//     setErrors({ ...errors, [index]: { ...errors[index], ...changedErrors } });
+//   };
+
+//   if (!schemasInitialized[index]) {
+//     setZodSchemas({ ...zodSchemas, [index]: schema });
+//     setSchemasInitialized({ ...schemasInitialized, [index]: true });
+//   }
+
+//   if (!storeInitialized[index]) {
+//     if (initialValue) {
+//       setStoreWrapper(initialValue, false);
+//     }
+//     setStoreInitialized({ ...storeInitialized, [index]: true });
+//   }
+
+//   return [store[index] as P & T, errors[index], setStoreWrapper] as [
+//     Partial<P>,
+//     ValidationError<P> | undefined,
+//     SetStore<P>,
+//   ];
+// };
