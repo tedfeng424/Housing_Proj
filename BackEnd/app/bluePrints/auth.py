17fbc0ae2becbf66a27a6775a73db15c5ec52c1c
nlin2 <nil021@ucsd.edu>
2020-12-23 16:24:49 -0800

    auth

--- BackEnd/app/bluePrints/auth.py
+++ BackEnd/app/bluePrints/auth.py
@@ -25,16 +25,12 @@ def login():
     session = current_app.config['DB_CONNECTION']
     user = check_exist(User, session, **{'email': requested_json['email']})
     if not user:
-        user = add_user(requested_json['name'], requested_json['email'],
-                        datetime.now(),
-                        "", "", "", "",
-                        session)
-        icon_path = './assets/profile_default_icons/'
-        selected_icon = random.choice(
-            os.listdir(icon_path))
-        path_name = "/".join([requested_json['email'],
-                              'profile', selected_icon])
-        upload_file_wname(icon_path+selected_icon, 'houseit', path_name)
+        # User doesn't exist
+        message, status = 'Continue with the setup', 100
+        response = generateResponse(elem=message, status=status)
+        response.set_cookie('access_token', access_token)
+        print(access_token)
+        return response
 
     login_session["user_id"] = user.id
     print(user.serialize)
@@ -62,4 +58,45 @@ def logout():
     if not client_token or (client_token != login_session["access_token"]):
         message, status = 'Logout is Forbidden due to wrong token', 403
         print(client_token, login_session["access_token"])
+    return generateResponse(elem=message, status=status)
+
+
+@authetication.route("/create_user", methods=["POST", "OPTIONS"])
+def create_user():
+    session = current_app.config['DB_CONNECTION']
+    if request.method == 'OPTIONS':
+        return generateResponse()
+    requested_json = request.json
+    if check_exist(User, session, **{'email': requested_json['email']}):
+        message, status = 'Already Created', 200
+        return generateResponse(elem=message, status=status)
+
+    user = add_user(requested_json['name'],
+                    requested_json['email'],
+                    datetime.now(),
+                    requested_json['phone'],
+                    requested_json["description"],
+                    requested_json["school_year"],
+                    requested_json["major"],
+                    session)
+    icon_path = './assets/profile_default_icons/'
+    selected_icon = random.choice(
+        os.listdir(icon_path))
+    path_name = "/".join([requested_json['email'],
+                          'profile', selected_icon])
+    upload_file_wname(icon_path+selected_icon, 'houseit', path_name)
+
+    json_response = {'user': requested_json['name'],
+                     'email': requested_json['email'],
+                     'access_token': login_session["access_token"],
+                     'message': 'Successfully created room.',
+                     'description': user.description,
+                     'phone': user.phone,
+                     'schoolYear': user.school_year,
+                     'major': user.major
+                     }
+    response = generateResponse(json_response)
+    response.set_cookie('access_token', login_session["access_token"])
+
+    message, status = 'Successfully Created', 201
     return generateResponse(elem=message, status=status)
