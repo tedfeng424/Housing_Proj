56afb210c0d92e84b5417e52407fb3066f64d3f7
yizong98 <yizong@ucsd.edu>
2020-12-30 18:50:43 -0800

    status code

--- BackEnd/app/bluePrints/auth.py
+++ BackEnd/app/bluePrints/auth.py
@@ -3,7 +3,7 @@ from flask import current_app, Blueprint, request, \
 from app.db.crud import check_exist, add_user
 from app.db.database_setup import User
 from datetime import datetime
-from app.util.util import handleOptions
+from app.util.util import generateResponse
 from app.util.aws.s3 import upload_file_wname
 import os
 import random
@@ -16,47 +16,91 @@ authetication = Blueprint('auth', __name__)
 def login():
     """Create anti-forgery state token."""
     if request.method == 'OPTIONS':
-        return handleOptions()
+        return generateResponse()
     access_token = "".join(random.choice(string.ascii_uppercase + string.digits)
                            for x in range(32))
     login_session["access_token"] = access_token
     requested_json = request.json
     # check in with db to see if user is new
     session = current_app.config['DB_CONNECTION']
-    if not check_exist(User, session, **{'email': requested_json['email']}):
+    user = check_exist(User, session, **{'email': requested_json['email']})
+    if not user:
+        # User doesn't exist
         add_user(requested_json['name'], requested_json['email'],
-                 datetime.now(),
-                 "", "", "", "",
-                 session)
-        icon_path = './assets/profile_default_icons/'
-        selected_icon = random.choice(
-            os.listdir(icon_path))
-        path_name = "/".join([requested_json['email'],
-                              'profile', selected_icon])
-        upload_file_wname(icon_path+selected_icon, 'houseit', path_name)
-    json_response = {}
-    json_response['user'] = requested_json['name']
-    json_response['email'] = requested_json['email']
-    json_response['access_token'] = access_token
-    json_response['message'] = 'Successfully created room.'
-    response = jsonify(json_response)
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
+            datetime.now(),
+            "", "", "", "",
+            session)
+        json_response = {'newUser': True}
+        response = generateResponse(json_response)
+        response.set_cookie('access_token', access_token)
+        print(access_token)
+        return response
+
+    login_session["user_id"] = user.id
+    print(user.serialize)
+    json_response = {'name': requested_json['name'],
+                     'email': requested_json['email'],
+                     'access_token': access_token,
+                     'message': 'Successfully created room.',
+                     'description': user.description,
+                     'phone': user.phone,
+                     'schoolYear': user.school_year,
+                     'major': user.major
+                     }
+    response = generateResponse(json_response)
+    response.set_cookie('access_token', access_token)
+    print(access_token)
     return response
 
 
 @authetication.route("/logout", methods=['POST', 'OPTIONS'])
 def logout():
     if request.method == 'OPTIONS':
-        return handleOptions()
+        return generateResponse()
     client_token = request.json.get('access_token')
-    if client_token and (client_token == login_session["access_token"]):
-        response = Response("Successful Logout!", status=200,
-                            mimetype='application/json')
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        return response
-    else:
+    message, status = 'Successful Logout!', 200
+    if not client_token or (client_token != login_session["access_token"]):
+        message, status = 'Logout is Forbidden due to wrong token', 403
         print(client_token, login_session["access_token"])
-        response = Response("Logout is Forbidden due to wrong token",
-                            status=403, mimetype='application/json')
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        return response
+    return generateResponse(elem=message, status=status)
+
+
+@authetication.route("/createUser", methods=["POST", "OPTIONS"])
+def create_user():
+    session = current_app.config['DB_CONNECTION']
+    if request.method == 'OPTIONS':
+        return generateResponse()
+    requested_json = request.json
+    if check_exist(User, session, **{'email': requested_json['email']}):
+        message, status = 'Already Created', 200
+        return generateResponse(elem=message, status=status)
+
+    user = add_user(requested_json['name'],
+                    requested_json['email'],
+                    datetime.now(),
+                    requested_json['phone'],
+                    requested_json["description"],
+                    requested_json["school_year"],
+                    requested_json["major"],
+                    session)
+    icon_path = './assets/profile_default_icons/'
+    selected_icon = random.choice(
+        os.listdir(icon_path))
+    path_name = "/".join([requested_json['email'],
+                          'profile', selected_icon])
+    upload_file_wname(icon_path+selected_icon, 'houseit', path_name)
+
+    json_response = {'name': requested_json['name'],
+                     'email': requested_json['email'],
+                     'access_token': login_session["access_token"],
+                     'message': 'Successfully created room.',
+                     'description': user.description,
+                     'phone': user.phone,
+                     'schoolYear': user.school_year,
+                     'major': user.major
+                     }
+    response = generateResponse(json_response)
+    response.set_cookie('access_token', login_session["access_token"])
+
+    message, status = 'Successfully Created', 201
+    return generateResponse(elem=message, status=status)
