f07fdef1e77683e9331d1a4ec64bf47e97dee2e9
yizong98 <yizong@ucsd.edu>
2021-01-15 15:13:59 -0800

    ll

--- BackEnd/app/util/search.py
+++ BackEnd/app/util/search.py
@@ -23,7 +23,7 @@ def compareMonth(early, late, curr):
 
 
 def checkOther(house, request):
-    return len(set(request) - set(house)) >= 1
+    return len(set(house) - set(request)) >= 1
 
 
 def search(room_json, session):
