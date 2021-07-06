1c19693ec395bee27c2cfdcc97dd4c702f19a4aa
Anya Chandorkar <anyachandorkar@Anyas-MacBook-Air-2.local>
2021-07-06 15:51:07 -0700

    boilerplate code

---
+++ BackEnd/app/filter/filter_db.py
@@ -0,0 +1,16 @@
+class DBFilter(object):
+  
+    def room_type(self,**criteria):
+       return []
+
+    def availability(self,**criteria):
+       return []
+
+    def price(self,**criteria):
+       return []
+    
+    def distance(self,**criteria):
+       return []
+
+
+
---
+++ BackEnd/app/filter/filter_json.py
@@ -0,0 +1,16 @@
+class JSONFilter(object):
+  
+    def room_type(self,**criteria):
+       return []
+
+    def availability(self,**criteria):
+       return []
+
+    def price(self,**criteria):
+       return []
+    
+    def distance(self,**criteria):
+       return []
+
+
+
