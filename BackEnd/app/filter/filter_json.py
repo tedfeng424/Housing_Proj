d186b010fb2dd3fdf38ab3649369eaaa7752b288
Ted Feng <tedf0424@gmail.com>
2021-08-10 15:48:09 -0700

    implement functions to filter rent and roomType for JSON files

--- BackEnd/app/filter/filter_json.py
+++ BackEnd/app/filter/filter_json.py
@@ -58,3 +58,66 @@ class JSONFilter(object):
 
     def filter(self):
         criteria = (self)
+
+    def clean_rent(rent):
+        """
+        Remove the signs from string rent and convert it to int
+        """
+        # assume rent is either int/float or str
+        if isinstance(rent, str):
+            return int(rent.replace('$', '').replace(',', ''))
+        else:
+            return rent
+
+    def filter_rent(criteria, index):
+        """
+        Check whether the rent of the listing at index satisfy the criteria
+        """
+        # some rent is range
+        rent = data[index]['rent'].split('-')
+        arr_rent = []
+        if len(rent) == 2:
+            arr_rent.append(clean_rent(rent[0]))
+            arr_rent.append(clean_rent(rent[1]))
+        else:
+            arr_rent.append(clean_rent(rent[0]))
+            arr_rent.append(clean_rent(rent[0]))
+
+        # find the intersection of the criteria and arr_rent
+        intersection = range(max(arr_rent[0], criteria[0]), min(
+            arr_rent[-1], criteria[-1])+1)
+        if len(intersection) == 0:
+            return False
+        else:
+            return True
+
+    def convert_input(input_str):
+        """
+        Convert input of roomType Criteria to a tuple of number and operator
+        """
+        if input_str[-1] == '+':
+            return (float(input_str[:-1]), operator.ge)
+        else:
+            return (float(input_str), operator.eq)
+
+    def process_roomType(roomType):
+        """
+        Process roomType of listings in JSON files
+        """
+        bed_index = roomType.index('Bed')
+        bath_index = roomType.index('Bath')
+        dict_roomType = {'Bedroom': float(
+            roomType[bed_index-2]), 'Bathroom': float(roomType[bath_index-2])}
+        return dict_roomType
+
+    def filter_roomType(criterias, index):
+        """
+        Check whether the roomType of the listing at index satisfy the criteria
+        """
+        criterias = {key: convert_input(val) for key, val in criterias.items()}
+        listing_roomType = process_roomType(data[index]['roomType'])
+        bool_bed = criterias['Bedroom'][1](
+            listing_roomType['Bedroom'], criterias['Bedroom'][0])
+        bool_bath = criterias['Bathroom'][1](
+            listing_roomType['Bathroom'], criterias['Bathroom'][0])
+        return bool_bed & bool_bath
