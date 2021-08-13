fe32fc54766b24f4e8358572510c85d99f1544a1
Anya Chandorkar <anyachandorkar@Anyas-MacBook-Air-2.local>
2021-08-13 14:05:22 -0700

    test verification update

--- BackEnd/tests/unit/test_filter.py
+++ BackEnd/tests/unit/test_filter.py
@@ -1,6 +1,7 @@
 import unittest
 from app.filter.filter import Filter
 
+
 class TestFilterVerification(unittest.TestCase):
     def setUp(self):
         self.filter = Filter(self)
@@ -25,21 +26,30 @@ class TestFilterVerification(unittest.TestCase):
         # testing false for invalid types
         self.assertFalse(self.filter.verify_room_type(["not a dictionary"]))
         self.assertFalse(self.filter.verify_room_type({}))
-        self.assertFalse(self.filter.verify_room_type({"Just one key": "value pair"}))
-        self.assertFalse(self.filter.verify_room_type({"Bedrooms": [], "Bathrooms": 9}))
-        self.assertFalse(self.filter.verify_room_type({"Bedrooms": 4, "Bathrooms": ""}))
+        self.assertFalse(self.filter.verify_room_type(
+            {"Just one key": "value pair"}))
+        self.assertFalse(self.filter.verify_room_type(
+            {"Bedrooms": [], "Bathrooms": 9}))
+        self.assertFalse(self.filter.verify_room_type(
+            {"Bedrooms": 4, "Bathrooms": ""}))
 
         # testing false for invalid bounds
-        self.assertFalse(self.filter.verify_room_type({"Inavlid Key Name 1": "1", "Invalid Key Name 2": "1"}))
-        self.assertFalse(self.filter.verify_room_type({"Bedrooms": "2.5", "Bathrooms": "3"}))
-        self.assertFalse(self.filter.verify_room_type({"Bedrooms": "-3", "Bathrooms": "1.5"}))
-        self.assertFalse(self.filter.verify_room_type({"Bedrooms": "4", "Bathrooms":" -2"}))
-        
+        self.assertFalse(self.filter.verify_room_type(
+            {"Inavlid Key Name 1": "1", "Invalid Key Name 2": "1"}))
+        self.assertFalse(self.filter.verify_room_type(
+            {"Bedrooms": "2.5", "Bathrooms": "3"}))
+        self.assertFalse(self.filter.verify_room_type(
+            {"Bedrooms": "-3", "Bathrooms": "1.5"}))
+        self.assertFalse(self.filter.verify_room_type(
+            {"Bedrooms": "4", "Bathrooms": " -2"}))
 
         # testing true for valid inputs
-        self.assertTrue(self.filter.verify_room_type({"Bedrooms": "1", "Bathrooms": "1"}))
-        self.assertTrue(self.filter.verify_room_type({"Bedrooms": "2", "Bathrooms": "2.5"}))
-        self.assertTrue(self.filter.verify_room_type({"Bedrooms": "3+", "Bathrooms": "3+"}))
+        self.assertTrue(self.filter.verify_room_type(
+            {"Bedrooms": "1", "Bathrooms": "1"}))
+        self.assertTrue(self.filter.verify_room_type(
+            {"Bedrooms": "2", "Bathrooms": "2.5"}))
+        self.assertTrue(self.filter.verify_room_type(
+            {"Bedrooms": "3+", "Bathrooms": "3+"}))
 
     def test_verify_distance(self):
         # testing false for invalid types
@@ -60,21 +70,32 @@ class TestFilterVerification(unittest.TestCase):
         # testing false for invalid types
         self.assertFalse(self.filter.verify_availability(["not a dictionary"]))
         self.assertFalse(self.filter.verify_availability({}))
-        self.assertFalse(self.filter.verify_availability({"Just one key": "value pair"}))
-        
+        self.assertFalse(self.filter.verify_availability(
+            {"Just one key": "value pair"}))
+
         # testing false for invalid bounds
-        self.assertFalse(self.filter.verify_availability({1:"key not str", "Year": "2020"}))
-        self.assertFalse(self.filter.verify_availability({"Month":"January", "Year": 2020}))
-        self.assertFalse(self.filter.verify_availability({"Not Month": "January", "Year": "1999"}))
-        self.assertFalse(self.filter.verify_availability({"Month": "December", "year": "2020"}))
-        self.assertFalse(self.filter.verify_availability({"Month": "Jason Derulo", "Year": "2020"}))
-        self.assertFalse(self.filter.verify_availability({"Month": "December", "Year": "twenty twenty"}))
-        self.assertFalse(self.filter.verify_availability({"Month": "December", "Year": "5"}))
+        self.assertFalse(self.filter.verify_availability(
+            {1: "key not str", "Year": "2020"}))
+        self.assertFalse(self.filter.verify_availability(
+            {"Month": "January", "Year": 2020}))
+        self.assertFalse(self.filter.verify_availability(
+            {"Not Month": "January", "Year": "1999"}))
+        self.assertFalse(self.filter.verify_availability(
+            {"Month": "December", "year": "2020"}))
+        self.assertFalse(self.filter.verify_availability(
+            {"Month": "Jason Derulo", "Year": "2020"}))
+        self.assertFalse(self.filter.verify_availability(
+            {"Month": "December", "Year": "twenty twenty"}))
+        self.assertFalse(self.filter.verify_availability(
+            {"Month": "December", "Year": "5"}))
 
         # testing true for valid inputs
-        self.assertTrue(self.filter.verify_availability({"Month": "March", "Year": "2013"}))
-        self.assertTrue(self.filter.verify_availability({"Month": "February", "Year": "2019"}))
-        self.assertTrue(self.filter.verify_availability({"Month": "October", "Year": "2015"}))
+        self.assertTrue(self.filter.verify_availability(
+            {"Month": "March", "Year": "2013"}))
+        self.assertTrue(self.filter.verify_availability(
+            {"Month": "February", "Year": "2019"}))
+        self.assertTrue(self.filter.verify_availability(
+            {"Month": "October", "Year": "2015"}))
 
     if __name__ == "__main__":
         unittest.main()
