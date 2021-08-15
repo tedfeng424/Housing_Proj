5c20033a3056494a7de07b522a8b2ebe0226013d
Ted Feng <tedf0424@gmail.com>
2021-08-15 16:37:46 -0700

    refined JSONFilter implementation  and add tests for JSONFilter

--- BackEnd/app/filter/filter_json.py
+++ BackEnd/app/filter/filter_json.py
@@ -3,8 +3,10 @@ import boto3
 import json
 import os
 from app.util.util import *
+from app.filter.filter import Filter
 import operator
 from datetime import datetime
+import numpy as np
 
 # load the aws credentials
 try:
@@ -46,12 +48,26 @@ def get_latest_data(bucket, dir):
 
 
 @attrs
-class JSONFilter(object):
-    filter_json = attrib()
-    data = get_latest_data()
+class JSONFilter(Filter):
+    #filter_json = attrib()
+    data = attrib(default=get_latest_data(BUCKET, DIR))
 
     def filter(self):
-        criteria = (self)
+        # checking whether the user inputs are valid or not
+        bool_rent = super().verify_price_range(self.criterias['rent'])
+        bool_room_type = super().verify_room_type(self.criterias['room_type'])
+        bool_distance = super().verify_distance(self.criterias['distance'])
+        bool_availability = super().verify_availability(
+            self.criterias['availability'])
+
+        # filter the listings
+        list_index = np.arange(len(self.data))
+        output_list = np.array(list(filter(lambda idx: (self.filter_rent(self.criterias['rent'], idx, bool_rent) and
+                                                        self.filter_room_type(self.criterias['room_type'], idx, bool_room_type) and
+                                                        self.filter_distance(self.criterias['distance'], idx, bool_distance) and
+                                                        self.filter_availability(self.criterias['availability'], idx, bool_availability)), list_index)))
+        result_list = output_list + 1
+        return result_list
 
     def clean_rent(self, rent):
         """
@@ -63,10 +79,15 @@ class JSONFilter(object):
         else:
             return rent
 
-    def filter_rent(self, criteria, index):
+    def filter_rent(self, criteria, index, bool_rent):
         """
         Check whether the rent of the listing at index satisfy the criteria
         """
+        # if the user input doesn't pass verification, don't filter by this criteria
+        # so always return True
+        if bool_rent == False:
+            return True
+
         # some rent is range
         rent = self.data[index]['rent'].split('-')
         arr_rent = []
@@ -100,32 +121,42 @@ class JSONFilter(object):
         """
         bed_index = room_type.index('Bed')
         bath_index = room_type.index('Bath')
-        dict_room_type = {'Bedroom': float(
-            room_type[bed_index-2]), 'Bathroom': float(room_type[bath_index-2])}
+        dict_room_type = {'Bedrooms': float(
+            room_type[bed_index-2]), 'Bathrooms': float(room_type[bath_index-2])}
         return dict_room_type
 
-    def filter_room_type(self, criterias, index):
+    def filter_room_type(self, criterias, index, bool_room_type):
         """
         Check whether the roomType of the listing at index satisfy the criteria
         """
+        # if the user input doesn't pass verification, don't filter by this criteria
+        # so always return True
+        if bool_room_type == False:
+            return True
+
         criterias = {key: self.convert_room_type_input(
             val) for key, val in criterias.items()}
         listing_room_type = self.process_room_type(
             self.data[index]['roomType'])
-        bool_bed = criterias['Bedroom'][1](
-            listing_room_type['Bedroom'], criterias['Bedroom'][0])
-        bool_bath = criterias['Bathroom'][1](
-            listing_room_type['Bathroom'], criterias['Bathroom'][0])
+        bool_bed = criterias['Bedrooms'][1](
+            listing_room_type['Bedrooms'], criterias['Bedrooms'][0])
+        bool_bath = criterias['Bathrooms'][1](
+            listing_room_type['Bathrooms'], criterias['Bathrooms'][0])
         return bool_bed & bool_bath
 
     def convert_distance_input(self, input_str):
         # converting string of "<20 mins" to tuple of num and operator
+        # substring of [1:-5] to get rid of the " mins" in the input
         if(input_str[0] == '<'):
             return (int(input_str[1:-5]), operator.le)
         else:
             return (int(input_str[1:-5]), operator.ge)
 
-    def filter_distance(self, criteria, index):
+    def filter_distance(self, criteria, index, bool_distance):
+        # if the user input doesn't pass verification, don't filter by this criteria
+        # so always return True
+        if bool_distance == False:
+            return True
         # comparing apartment distances to input and returning true or false
         criteria = self.convert_distance_input(criteria)
         listing_distance = int(self.data[index]['distance'][0:-5])
@@ -139,16 +170,19 @@ class JSONFilter(object):
         date_str = month + " 1 " + year
         return datetime.strptime(date_str, "%B %d %Y")
 
-    def filter_availability(self, criteria, index):
+    def filter_availability(self, criteria, index, bool_availability):
+        # if the user input doesn't pass verification, don't filter by this criteria
+        # so always return True
+        if bool_availability == False:
+            return True
         # converts both input and listing to datetime object and compares
         criteria = self.convert_availability_input(criteria)
         listing_availability = self.data[index]['availability'].lower()
         if((listing_availability == "now") or (listing_availability == "today")):
             return True
+        listing_availability = datetime.strptime(
+            listing_availability, "%m/%d/%Y")
+        if(listing_availability <= criteria):
+            return True
         else:
-            listing_availability = datetime.strptime(
-                listing_availability, "%m/%d/%Y")
-            if(listing_availability < criteria):
-                return True
-            else:
-                return False
+            return False
--- BackEnd/tests/unit/test_filter_json.py
+++ BackEnd/tests/unit/test_filter_json.py
@@ -1,8 +1,34 @@
 import unittest
 import boto3
+from app.filter.filter_json import JSONFilter
+import json
 
 
 class TestJsonFilterOperations(unittest.TestCase):
-    def test_distance(self):
-        assert type(distance_array[0])
-        assert distance_array[1] > distance_array[0]
+
+    def setUp(self):
+        f = open('tests/unit/json_filter_test_data.json', )
+        self.data = json.load(f)
+
+    def test_rent(self):
+        criteria = {'rent': [1000, 2500], 'room_type': None,
+                    'distance': None, 'availability': None}
+        # creating a JSONFilter object with criterias and data as attributes
+        json_filter_rent = JSONFilter(criterias=criteria, data=self.data)
+        # adjusted indexed for json file
+        listing_ids = json_filter_rent.filter()-1
+        # go through all the output indexes
+        for index in listing_ids:
+            # convert the format of rent from json files
+            rent = self.data[index]['rent'].split('-')
+            arr_rent = []
+            if len(rent) == 2:
+                arr_rent.append(json_filter_rent.clean_rent(rent[0]))
+                arr_rent.append(json_filter_rent.clean_rent(rent[1]))
+            else:
+                arr_rent.append(json_filter_rent.clean_rent(rent[0]))
+                arr_rent.append(json_filter_rent.clean_rent(rent[0]))
+
+            # check whether all the filtered listings satisfies the criteria
+            self.assertTrue(arr_rent[0] >= 1000)
+            self.assertTrue(arr_rent[1] <= 2500)
