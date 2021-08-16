6715267e24c1e99b7b00ff5a4d799ecb56bdcdb1
Anya Chandorkar <anyachandorkar@Anyas-MacBook-Air-2.local>
2021-08-16 12:16:00 -0700

    tests made for availability and distance

--- BackEnd/tests/unit/test_filter_json.py
+++ BackEnd/tests/unit/test_filter_json.py
@@ -2,6 +2,7 @@ import unittest
 import boto3
 from app.filter.filter_json import JSONFilter
 import json
+from datetime import datetime
 
 
 class TestJsonFilterOperations(unittest.TestCase):
@@ -60,7 +61,7 @@ class TestJsonFilterOperations(unittest.TestCase):
             self.assertTrue(arr_rent[1] <= 2500)
 
     def test_rent_no_max(self):
-        # when min rent is not specified
+        # when max rent is not specified
         criteria = {'rent': [1000, None], 'room_type': None,
                     'distance': None, 'availability': None}
         # creating a JSONFilter object with criterias and data as attributes
@@ -84,7 +85,7 @@ class TestJsonFilterOperations(unittest.TestCase):
             self.assertTrue(arr_rent[1] <= 99999)
 
     def test_no_rent(self):
-        # when min rent is not specified
+        # when rent is not specified
         criteria = {'rent': None, 'room_type': None,
                     'distance': None, 'availability': None}
         # creating a JSONFilter object with criterias and data as attributes
@@ -106,3 +107,91 @@ class TestJsonFilterOperations(unittest.TestCase):
             # check whether all the filtered listings satisfies the criteria
             self.assertTrue(arr_rent[0] >= 0)
             self.assertTrue(arr_rent[1] <= 99999)
+
+    def test_distance(self):
+        # checking with a valid input of distance
+        criteria = {'rent': None, 'room_type': None,
+                'distance': "<20 mins", 'availability': None}
+        #creating a JSONFilter object with criterias and data as attributes
+        json_filter_distance = JSONFilter(criterias=criteria, data=self.data)
+        # adjusted indexed for json file
+        listing_ids = json_filter_distance.filter()-1
+        # go through all the output indexes
+        for index in listing_ids:
+            arr_distance = []
+            # convert the format of distance from json files
+            distance = int(self.data[index]['distance'][1:-5])
+            arr_distance.append(distance)
+        # check whether all the filtered listings satisfies the criteria
+        self.assertTrue(arr_distance>= 0)
+        self.assertTrue(arr_distance<= 20)
+
+    def test_no_distance(self):
+        # checking with no input of distance to verify default value works
+        criteria = {'rent': None, 'room_type': None,
+                'distance': None, 'availability': None}
+        #creating a JSONFilter object with criterias and data as attributes
+        json_filter_distance = JSONFilter(criterias=criteria, data=self.data)
+        # adjusted indexed for json file
+        listing_ids = json_filter_distance.filter()-1
+        # go through all the output indexes
+        for index in listing_ids:
+            arr_distance = []
+            # convert the format of distance from json files
+            distance = int(self.data[index]['distance'][1:-5])
+            arr_distance.append(distance)
+        # check whether all the filtered listings satisfies the criteria
+        self.assertTrue(arr_distance>= 0)
+        self.assertTrue(arr_distance<= 1000)
+
+    def test_availability(self):
+        # checking with a valid input of availability
+        criteria = {'rent': None, 'room_type': None,
+                'distance': None, 'availability': {"Month": "January", "Year": "2021"}}
+        
+        #creating a JSONFilter object with criterias and data as attributes
+        json_filter_availability = JSONFilter(criterias=criteria, data=self.data)
+        # adjusted indexed for json file
+        listing_ids = json_filter_availability.filter()-1
+        # go through all the output indexes
+        for index in listing_ids:
+            arr_availability = []
+            # convert the format of availability from json files
+            availability = self.data[index]['availability']
+            if((availability.lower() == 'now') or (availability.lower() == 'today')):
+                availability = datetime.now
+            else:
+                availability = datetime.strptime(availability, "%m/%d/%Y")
+            arr_availability.append(availability)
+        # check whether all the filtered listings satisfies the criteria
+        avail_crit = json_filter_availability.convert_availability_input(criteria.get('availability'))
+        self.assertTrue(arr_availability<=avail_crit)
+
+    def test_no_availability(self):
+        # checking with no input of availability to verify default value works
+        criteria = {'rent': None, 'room_type': None,
+                'distance': None, 'availability': None}
+        
+        #creating a JSONFilter object with criterias and data as attributes
+        json_filter_availability = JSONFilter(criterias=criteria, data=self.data)
+        # adjusted indexed for json file
+        listing_ids = json_filter_availability.filter()-1
+        # go through all the output indexes
+        for index in listing_ids:
+            arr_availability = []
+            # convert the format of availability from json files
+            availability = self.data[index]['availability']
+            if((availability.lower() == 'now') or (availability.lower() == 'today')):
+                availability = datetime.now
+            else:
+                availability = datetime.strptime(availability, "%m/%d/%Y")
+            arr_availability.append(availability)
+        # check whether all the filtered listings satisfies the criteria
+        avail_crit = json_filter_availability.convert_availability_input(
+            {"Month": datetime.now().strftime("%B"), "Year": str(datetime.now().year+100)})
+        self.assertTrue(arr_availability<=avail_crit)
+
+
+    
+
+
