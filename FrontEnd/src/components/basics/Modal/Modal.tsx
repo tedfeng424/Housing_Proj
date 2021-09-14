735a0bef11384d57975420627452579fbf1f845c
Anthony Manrique <55559651+AnthonyM456@users.noreply.github.com>
2021-09-13 23:26:44 -0700

    New modal (#100)
    * Regular Rebase from Amit's master
    
    This PR contains the features for the Homehub V2
    
    * added props
    
    * updated modal component
    
    * flex direction removal
    
    * Code revisions + logic changes
    
    * materialUI onClose
    
    * alt removed + comment header revised
    
    Co-authored-by: Yijian Zong <yijianzong@gmail.com>

--- FrontEnd/src/components/basics/Modal/Modal.tsx
+++ FrontEnd/src/components/basics/Modal/Modal.tsx
@@ -5,15 +5,39 @@ import {
 } from '@material-ui/core/Modal';
 import cn from 'classnames';
 import styles from './Modal.module.scss';
+import { Subtitle1, Button } from '@basics';
+import { miscIcons } from '@icons';
+
+export type ImageTypeName = { src: string; alt?: string };
 
 interface ModalProps
   extends Omit<MaterialUIModalProps, 'disableBackdropClick' | 'children'> {
   children?: (ReactElement | undefined | false)[];
   size?: 'md' | 'lg';
+  title?: string;
+  caption?: string;
+  modalGraphic?: string | ImageTypeName;
 }
 
+/**
+ * Modal component. Used for popups. size is 'md' (medium) by default
+ *
+ * Can optionally provide a a modalGraphic, a visual/image for the popup,
+ * a title, displayed under modal graphic (if provided), and a caption,
+ * brief explanation of modal.
+ *
+ * children can be additional buttons/components unique to specific modal
+ *
+ * onClose needs to be provied in order to close the popup (with either
+ * the exit button or esc)
+ *
+ */
 const Modal: FunctionComponent<ModalProps> = ({
+  title,
+  caption,
+  modalGraphic,
   children,
+  onClose,
   open,
   className,
   size = 'md',
@@ -23,6 +47,7 @@ const Modal: FunctionComponent<ModalProps> = ({
 }) => {
   return (
     <MaterialUIModal
+      onClose={onClose}
       open={open}
       keepMounted={keepMounted}
       {...passedProps}
@@ -30,7 +55,35 @@ const Modal: FunctionComponent<ModalProps> = ({
       disableBackdropClick
     >
       <div className={cn(styles.modal, styles[size], className)}>
-        <>{children}</>
+        <div>
+          <Button variant="wrapper" className={styles.close} onClick={onClose}>
+            <miscIcons.orangeX />
+          </Button>
+        </div>
+
+        {modalGraphic && (
+          <div className={styles.modalGraphic}>
+            {typeof modalGraphic === 'string' ? (
+              <img src={modalGraphic} />
+            ) : (
+              <img src={modalGraphic.src} alt={modalGraphic.alt} />
+            )}
+          </div>
+        )}
+
+        {title && (
+          <div className={cn(styles.text, styles.title)}>
+            <h4>{title}</h4>
+          </div>
+        )}
+
+        {caption && (
+          <Subtitle1 className={cn(styles.text, styles.caption)}>
+            {caption}
+          </Subtitle1>
+        )}
+
+        {children}
       </div>
     </MaterialUIModal>
   );
